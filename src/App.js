5316ed81b483bbd3964819ea5920d0f3b2e837b3
Ritdxer <ritdxer@gmail.com>
2020-03-09 21:49:40 +0200

    add Person component and toggle persons visibility

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Roman', age: 26 },
+      { name: 'Natalia', age: 26 },
+      { name: 'Andrii', age: 28 }
+    ],
+    showPersons: false
+  };
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 26 },
+        { name: 'Roman', age: 26 },
+        { name: 'Andrii', age: 28 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 26 },
+        { name: 'Roman', age: 26 },
+        { name: 'Andrii', age: 28 }
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'Still Roman')}
+            changed={this.nameChangedHandler}>Hobbies: Photography</Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age} />
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-      <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
